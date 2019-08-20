faca4a8ad8176cd82675ceceafbeac80743786d0
Fooxboy <Fooxboy@yandex.ru>
2019-08-20 22:12:58 +0300

    Добавьте файлы проекта.

---
+++ Fooxboy.MusicVKDownloader/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonServiceLocator;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+
+namespace Fooxboy.MusicVKDownloader.ViewModels
+{
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                // Create design time view services and models
+            }
+            else
+            {
+                // Create run time view services and models
+            }
+
+            //Register your services used here
+            SimpleIoc.Default.Register<INavigationService, NavigationService>();
+            SimpleIoc.Default.Register<HelloWorldViewModel>();
+
+        }
+
+        public HelloWorldViewModel StartPageInstance
+        {
+            get => ServiceLocator.Current.GetInstance<HelloWorldViewModel>();
+        }
+
+        // <summary>
+        // The cleanup.
+        // </summary>
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+    }
+}
